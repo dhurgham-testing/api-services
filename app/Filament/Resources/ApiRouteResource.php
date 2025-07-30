61082cc79c7601a34068e63245f380c085157753
dhurgham <dhurgham.oday@miswag.com>
2025-07-30 14:17:37 +0300

    update

--- app/Filament/Resources/ApiRouteResource.php
+++ app/Filament/Resources/ApiRouteResource.php
@@ -38,7 +38,6 @@ class ApiRouteResource extends Resource
                     ->live(onBlur: true)
                     ->placeholder('Select a middleware first...')
                     ->afterStateUpdated(function ($state, $set) {
-                        \Log::info("Middleware updated to: {$state}");
                         $set('service_group', '');
                         $set('controller_name', '');
                         $set('method_name', '');
@@ -51,7 +50,6 @@ class ApiRouteResource extends Resource
                     ->label('Service Group (e.g., youtube, spotify)')
                     ->live(onBlur: true)
                     ->afterStateUpdated(function ($state, $set) {
-                        \Log::info("Service group updated to: {$state}");
                         $set('controller_name', '');
                         $set('method_name', '');
                     }),
@@ -62,9 +60,7 @@ class ApiRouteResource extends Resource
                     ->label('Controller Name')
                     ->options(function ($get) {
                         $service_group = $get('service_group');
-
                         if (!$service_group) {
-                            \Log::info("No service group selected for controller options");
                             return [];
                         }
 
@@ -74,28 +70,20 @@ class ApiRouteResource extends Resource
                             ? app_path("Http/Controllers/" . ucfirst($middleware))
                             : app_path("Http/Controllers/" . ucfirst($middleware) . "/" . ucfirst($service_group));
 
-                        \Log::info("Looking for controllers in path: {$base_path}");
-                        \Log::info("Middleware: {$middleware}, Service Group: {$service_group}");
-
                         if (is_dir($base_path)) {
                             $files = glob($base_path . "/*.php");
-                            \Log::info("Found files: " . implode(', ', $files));
                             foreach ($files as $file) {
                                 $filename = basename($file, '.php');
                                 $controllers[ucfirst($filename)] = ucfirst($filename);
                             }
-                        } else {
-                            \Log::warning("Directory does not exist: {$base_path}");
                         }
 
-                        \Log::info("Available controllers: " . implode(', ', array_keys($controllers)));
                         return $controllers;
                     })
                     ->searchable()
                     ->placeholder('Select a controller...')
                     ->live(onBlur: true)
                     ->afterStateUpdated(function ($state, $set) {
-                        \Log::info("Controller selected: {$state}");
                         $set('method_name', '');
                     }),
 
@@ -113,51 +101,33 @@ class ApiRouteResource extends Resource
                         $middleware = $get('middleware') ?? 'api';
                         $controller_name = $get('controller_name');
                         $service_group = $get('service_group');
-
-                        \Log::info("Method options requested - Middleware: {$middleware}, Controller: {$controller_name}, Service Group: {$service_group}");
-
-                        if (!$controller_name || !$service_group) {
-                            \Log::warning("Missing controller_name or service_group for method options");
-                            return [];
-                        }
-
                         $controller_class = $service_group == "/" ? "App\\Http\\Controllers\\" . ucfirst($middleware) . "\\{$controller_name}" : "App\\Http\\Controllers\\" . ucfirst($middleware) ."\\" . ucfirst($service_group) . "\\{$controller_name}";
                         $controller_name_parts = explode('\\', $controller_class);
                         $last_part = end($controller_name_parts);
                         $controller_class = str_replace($last_part, ucfirst($last_part), $controller_class);
-                        
-                        \Log::info("Initial controller class: {$controller_class}");
-                        
-                        // Try multiple case variations if class doesn't exist
                         $possible_classes = [
                             $controller_class,
                             str_replace(ucfirst($last_part), $last_part, $controller_class), // original case
                             str_replace(ucfirst($last_part), strtolower($last_part), $controller_class), // lowercase
                             str_replace(ucfirst($last_part), strtoupper($last_part), $controller_class), // uppercase
                         ];
-                        
-                        \Log::info("Trying possible classes: " . implode(', ', $possible_classes));
-                        
+
                         $found_class = null;
                         foreach ($possible_classes as $class) {
                             if (class_exists($class)) {
                                 $found_class = $class;
-                                \Log::info("Found existing class: {$class}");
                                 break;
                             }
                         }
-                        
+
                         if (!$found_class) {
-                            \Log::warning("No class found. Tried: " . implode(', ', $possible_classes));
                             return [];
                         }
-                        
+
                         $controller_class = $found_class;
-                        \Log::info("Using controller class: {$controller_class}");
 
                         try {
                             $reflection = new \ReflectionClass($controller_class);
-                            \Log::info("Reflection successful for: {$controller_class}");
 
                             $public_methods = collect($reflection->getMethods(\ReflectionMethod::IS_PUBLIC))
                                 ->filter(function ($method) use ($controller_class) {
@@ -167,8 +137,6 @@ class ApiRouteResource extends Resource
                                 })
                                 ->mapWithKeys(fn($method) => [$method->getName() => $method->getName()]);
 
-                            $method_names = $public_methods->keys()->toArray();
-                            \Log::info("Found methods: " . implode(', ', $method_names));
                             return $public_methods->toArray();
                         } catch (\ReflectionException $e) {
                             \Log::error("Reflection failed for {$controller_class}: " . $e->getMessage());
@@ -176,10 +144,7 @@ class ApiRouteResource extends Resource
                         }
                     })
                     ->searchable()
-                    ->placeholder('Select a controller first...')
-                    ->afterStateUpdated(function ($state, $set) {
-                        \Log::info("Method selected: {$state}");
-                    }),
+                    ->placeholder('Select a controller first...'),
 
         Forms\Components\Select::make('http_method')
                     ->required()
